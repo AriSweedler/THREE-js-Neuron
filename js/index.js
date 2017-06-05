5495b178b7cf8106cf5fe3ff37176180e9ab5bc9
Ari Sweedler <ari@sweedler.com>
2017-06-05 10:32:25 -0700

    Added a "player" group to host the camera
    Clicking on the screen, you can look around the world. There’s just
    five spheres above one’s head right now. Movement is next.

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ function init() {
 	for(let i = 0; i < 5; i++) {
 		let n = new Neuron(i);
 		n.mesh.position.x = (-75) + (40*i); //+ (-5 + Math.random()*10);
-		n.mesh.position.y = (0); + //(-3 + Math.random()*6);
+		n.mesh.position.y = (50); //(-3 + Math.random()*6);
 
 		addToMyGame(n);
 	}
@@ -37,6 +37,7 @@ function createScene() {
 	this.HEIGHT = container.innerHeight;
 
 	createSceneInstance();
+	createPlayer();
 	createCameraInstance();
 	createRendererInstance();
 	createClickyShit();
@@ -48,7 +49,7 @@ function createScene() {
 
 /**************************************/
 /********init helper functions*********/
-var scene, camera, renderer,
+var scene, camera, player, renderer, centerOfScene,
 		projector, raycaster;
 function createSceneInstance() {
 	scene = new THREE.Scene();
@@ -68,6 +69,7 @@ function createLights() {
 	hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9)
 	scene.add(hemisphereLight);
 }
+
 function createCameraInstance() {
 	// Create the camera
 	let aspectRatio = WIDTH / HEIGHT;
@@ -81,9 +83,10 @@ function createCameraInstance() {
 		farPlane
 	);
 
-	let centerOfScene = {x: 0, y: 0, z: 0};
+	centerOfScene = {x: 0, y: 0, z: 0};
 	// need three numeric parameters for camera.position.set
 	// Make sure to move the camera back 200 units from the center of the scene so we can actually see
+	camera.LookVector = new THREE.Vector3(0, 0, 200);
 	camera.position.set(centerOfScene.x, centerOfScene.y, centerOfScene.z + 200);
 	camera.lookAt(new THREE.Vector3(centerOfScene.x, centerOfScene.y, centerOfScene.z));
 	//need a THREE.Vector3 object as a parameter for camera.lookAt
@@ -93,6 +96,67 @@ function createCameraInstance() {
 	camera.position.target.x = centerOfScene.x;
 	camera.position.target.y = centerOfScene.y;
 	camera.position.target.z = centerOfScene.z+200;
+}
+function createPlayer() {
+	player = new THREE.Group();
+
+	player.up = new THREE.Mesh(
+		new THREE.CylinderGeometry(3, 3, 10, 3),
+		new THREE.MeshBasicMaterial()
+	);
+	player.up.ball = new THREE.Mesh(
+		new THREE.SphereGeometry(1, 10, 10),
+		new THREE.MeshBasicMaterial()
+	);
+	player.up.ball.position.y += 5;
+	player.up.add(player.up.ball);
+	player.up.material.color.set(0x00ff00);
+	player.up.position.y += 5;
+	//up.material.wireframe = true;
+	player.up.geometry.applyMatrix(new THREE.Matrix4().makeRotationY(-Math.PI/2));
+	player.add(player.up);
+
+	player.forward = new THREE.Mesh(
+		new THREE.CylinderGeometry(3, 3, 10, 3),
+		new THREE.MeshBasicMaterial()
+	);
+	player.forward.ball = new THREE.Mesh(
+		new THREE.SphereGeometry(1, 10, 10),
+		new THREE.MeshBasicMaterial()
+	);
+	player.forward.ball.position.z -= 5;
+	player.forward.add(player.forward.ball);
+	player.forward.material.color.set(0xff0000);
+	player.forward.position.z -= 5;
+	//player.forward.material.wireframe = true;
+	player.forward.geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+	player.add(player.forward);
+
+	player.sideways = new THREE.Mesh(
+		new THREE.CylinderGeometry(3, 3, 10, 3),
+		new THREE.MeshBasicMaterial()
+	);
+	player.sideways.ball = new THREE.Mesh(
+		new THREE.SphereGeometry(1, 10, 10),
+		new THREE.MeshBasicMaterial()
+	);
+	player.sideways.ball.position.x -= 5;
+	player.sideways.add(player.sideways.ball);
+	player.sideways.material.color.set(0x0000ff);
+	player.sideways.position.x -= 5;
+	//player.sideways.material.wireframe = true;
+	player.sideways.geometry.applyMatrix(new THREE.Matrix4().makeRotationZ(-Math.PI/2));
+	player.add(player.sideways);
+
+	player.ball = new THREE.Mesh(
+		new THREE.SphereGeometry(1, 10, 10),
+		new THREE.MeshBasicMaterial()
+	);
+	player.add(player.ball);
+
+	player.position.set(0, 0, 0);
+	player.scale.set(2, 2, 2);
+	scene.add(player);
 }
 function createRendererInstance() {
 	// Create the renderer
@@ -117,9 +181,9 @@ function createControls() {
 	//console.log("creating controls");
 
 	// Hook pointer lock state change events
-	document.addEventListener('pointerlockchange', pointerLockChange, false);
-	document.addEventListener('mozpointerlockchange', pointerLockChange, false);
-	document.addEventListener('webkitpointerlockchange', pointerLockChange, false);
+	document.addEventListener('pointerlockchange', lockChangeAlert, false);
+	document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+	document.addEventListener('webkitpointerlockchange', lockChangeAlert, false);
 
 	// Hook mouse move events
 	//document.addEventListener("mousemove", lockedMouseMove, false);
@@ -128,39 +192,37 @@ function createControls() {
     'mozPointerLockElement' in document ||
     'webkitPointerLockElement' in document;
 
-	console.log(havePointerLock + " is what I say about pointer lock.");
-
 	let element = container;
 	// Ask the browser to lock the pointer
-	element.requestPointerLock = element.requestPointerLock ||
-			element.mozRequestPointerLock ||
-			element.webkitRequestPointerLock;
-	console.log(element.requestPointerLock);
-	element.requestPointerLock();
+	container.requestPointerLock = container.requestPointerLock ||
+			container.mozRequestPointerLock ||
+			container.webkitRequestPointerLock;
+	container.onclick = () => {container.requestPointerLock();}
 
-	// // Ask the browser to release the pointer
-	// document.exitPointerLock = document.exitPointerLock ||
-	// 		document.mozExitPointerLock ||
-	// 		document.webkitExitPointerLock;
-	// document.exitPointerLock();
+	// Ask the browser to release the pointer
+	document.exitPointerLock = document.exitPointerLock ||
+			document.mozExitPointerLock ||
+			document.webkitExitPointerLock;
+	document.exitPointerLock();
 }
-function pointerLockChange() {
-	console.log("I am happy");
-	if (document.pointerLockElement === requestedElement ||
-  		document.mozPointerLockElement === requestedElement ||
-  		document.webkitPointerLockElement === requestedElement) {
+function lockChangeAlert() {
+	if (document.pointerLockElement === container ||
+  		document.mozPointerLockElement === container ||
+  		document.webkitPointerLockElement === container) {
   	// Pointer was just locked, enable the mousemove listener
+		container.onclick = null;
   	document.addEventListener("mousemove", lockedMouseMove, false);
 	} else {
-		console.log("what is unlock hook");
 	  // Pointer was just unlocked, disable the mousemove listener
 	  document.removeEventListener("mousemove", lockedMouseMove, false);
-	  this.unlockHook(this.element);
+		console.log("mouse unlocked");
+		console.log(player);
+		container.onclick = () => {container.requestPointerLock();}
+	  //this.unlockHook(container);
 	}
 }
 function lockedMouseMove(e) {
-	console.log("locked mouse move");
-
+	//console.log("Locked mouse move");
 	var movementX = e.movementX ||
 		e.mozMovementX          ||
 		e.webkitMovementX       ||
@@ -170,6 +232,15 @@ function lockedMouseMove(e) {
 		e.mozMovementY      ||
 		e.webkitMovementY   ||
 		0;
+
+	if(movementX != 0) {
+		console.log("yaw" + movementX);
+		player.rotation.y -= (movementX/100);
+	}
+	if(movementY != 0) {
+		console.log("pitch" + movementY);
+		player.rotation.x -= (movementY/100);
+	}
 }
 
 function update() {
@@ -179,8 +250,15 @@ function update() {
 			scene.objects[i].update();
 		}
 
-		//update camera
-		moveCamera();
+		//update the "player"'s position (and the camera's position)
+		movePlayer();
+		console.log(player.ball.position.x + " " + player.ball.position.y + " " + player.ball.position.z);
+		//console.log(player.forward.ball.position.x + " " + player.forward.ball.position.y + " " + player.forward.ball.position.z);
+		camera.position.set(player.ball.position.x, player.ball.position.y, player.ball.position.z);
+
+		camera.rotation.x = player.rotation.x;
+		camera.rotation.y = player.rotation.y;
+
 
 		//update mouse stuff
 
@@ -225,16 +303,22 @@ function addToMyGame(myObj) {
 	myObj.init();
 }
 
-function moveCamera() {
-	if (keys[65]) {camera.position.target.x -= camera.moveSpeed;}
-	if (keys[87]) {camera.position.target.y += camera.moveSpeed;}
-	if (keys[68]) {camera.position.target.x += camera.moveSpeed;}
-	if (keys[83]) {camera.position.target.y -= camera.moveSpeed;}
-
-	for(var n in camera.position.target) {
-		let delta = camera.position.target[n] - camera.position[n];
-		let v = delta*.1;
-		if(Math.abs(v) < camera.moveSpeed/10) {camera.position.target[n] = camera.position[n];}
-		camera.position[n] += v;
-	}
+function movePlayer() {
+	// let delta = {};
+	// for(var n in camera.position) {
+	// 	delta[n] = camera.position[n] - centerOfScene[n];
+	// }
+	// // need three numeric parameters for camera.position.set
+	//
+	// if (keys[65]) {camera.position.target.x -= camera.moveSpeed;}//left
+	// if (keys[87]) {camera.position.target.y += camera.moveSpeed;}//up
+	// if (keys[68]) {camera.position.target.x += camera.moveSpeed;}//right
+	// if (keys[83]) {camera.position.target.y -= camera.moveSpeed;}//down
+	// // //
+	// for(var n in camera.position.target) {
+	// 	let delta = camera.position.target[n] - camera.position[n];
+	// 	let v = delta*.1;
+	// 	if(Math.abs(v) < camera.moveSpeed/10) {camera.position.target[n] = camera.position[n];}
+	// 	camera.position[n] += v;
+	// }
 }
